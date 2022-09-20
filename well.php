8033f7a29d4336976158791ebf57e761ce00a320
sagorislam2 <sagor7239@gmail.com>
2022-09-20 11:40:28 +0600

    this is my second comit

---
+++ well.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+Wellcome <?php echo $_POST["name"];?><br>
+your email address is <?php echo $_POST["email"];?><br>
+Your country is <?php echo $_POST["country"];?>
+
+
+
+
+</body>
+
+
+
+
+</html>
